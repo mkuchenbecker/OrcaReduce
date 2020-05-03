407e999436192bd1dfb7d38fbec51dc31613b892
Mike Kuchenbecker <mkuch90@gmail.com>
2020-05-03 06:55:02 -0700

    .

--- orcareduce/director/director.go
+++ orcareduce/director/director.go
@@ -3,33 +3,41 @@ package prod
 import (
 	"github.com/mkuchenbecker/orcareduce/orcareduce"
 	"github.com/mkuchenbecker/orcareduce/orcareduce/exceptions"
-	"golang.org/pkg/errors"
+	"github.com/pkg/errors"
 )
 
-type queueDirector struct {
-	actors []orcareduce.Reactor
+
+
+type config struct {
+	retries int
+	backoffFactor int
 }
 
-func (d * queueDirector) GetNextActor() (actor orcareduce.Reactor, err error) {
-	if len(d.actors) == 0 {
-		return nil, exceptions.ErrActorsDepleted
-	}
-	actor = d.actors[0]
-	return actor,nil
+func (c *config) Backoff(retry int) time.Duration {
+	return math.Max(1,2) * time.Second
+}
+
+func (c *config) Retries() int {
+
 }
 
 
-func (d *queueDirector) Direct() (err error) {
+type director struct {
+	actor orcareduce.Reactor
+	cfg config
+}
+
+func (d *director) Direct() (err error) {
 	defer func() {
 		err = errors.Wrap(err,"unable to direct")
-	}
+	}()
 	actor, err := d.GetNextActor()
 	if err != nil {
 		return  err
 	}
 	err = actor.Preconditions()
 	if err != nil {
-		return err
+		return exceptions.PreconditionError(err.Error())
 	}
 	err = actor.Act()
 	if err != nil {
@@ -43,3 +51,12 @@ func (d *queueDirector) Direct() (err error) {
 
 	return nil
 }
+
+
+func (d *director) Run() {
+	for i:= 0; i < d.config.Retries(); i++ {
+
+
+	}
+}
+
--- orcareduce/exceptions/errors.go
+++ orcareduce/exceptions/errors.go
@@ -4,4 +4,12 @@ import "fmt"
 
 var (
 	ErrActorsDepleted = fmt.Errorf("actors depleted")
+	ErrPreconditionsNotMet = fmt.Errorf("preconditions not met")
 )
+
+
+type PreconditionError string
+
+func (err PreconditionError) Error() string {
+	return fmt.Sprintf("preconditions not met: %s", err)
+}
